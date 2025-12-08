f2c0abc6c5e582a567c9d060d4029bd231d90d7d
Patrick Gougeon <patrickgougeoncouto@gmail.com>
2025-12-08 13:19:36 -0300

    Começando a lidar com banco de dados...

---
+++ api.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+USUARIO = os.getenv('DB_USUARIO')
+SENHA = os.getenv('DB_SENHA')
+HOST = os.getenv('DB_HOST')
+BANCO = os.getenv('DB_NOME')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{USUARIO}:{SENHA}@{HOST}/{BANCO}'
+
+db = SQLAlchemy(app)
+
+class Especialidade(db.Model):
+    __tablename__ = 'especialidade'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    
+    medicos = db.relationship('Medico', backref='possui')
+    cirurgias = db.relationship('Cirurgia', backref='possui')
+
+class Cirurgia(db.Model):
+    __tablename__ = 'cirurgia'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    
+    id_especialidade = db.Column(db.Integer, db.ForeighKey('id_especialidade'))
+
+class Medico(db.Model):
+    __tablename__ = 'medico'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    tipo = db.Column(db.String(100))
+
+    id_especialidade = db.Column(db.Integer, db.ForeighKey('id_especialidade')) 
+    
+class Responsavel(db.Model):
+    __tablename__ = 'responsavel'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+
+class Plano(db.Model):
+    __tablename__ = 'plano'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from tags_api import api_bp
-
-app = Flask(__name__)
-
-app.register_blueprint(api_bp, url_prefix='/api')
-
-if __name__ == '__main__':
-    app.run(debug=True)
